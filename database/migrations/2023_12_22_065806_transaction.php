60aed8fb29b597d4e0caf2113401413efd53ed9f
janicelevina1 <154639144+janicelevina1@users.noreply.github.com>
2023-12-23 17:40:03 +0700

    first

---
+++ database/migrations/2023_12_22_065806_transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('kategori', 50);
+            $table->double('nominal');
+            $table->string('tujuan');
+            $table->char('account_id', 16);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
